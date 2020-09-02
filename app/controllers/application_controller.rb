45c513de9a9f52bb018b92a1f61e2324700f7ae7
Tomas Landovsky <landovsky@gmail.com>
2020-09-02 22:30:51 +0200

    Basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with name: ENV['BASIC_AUTH_USERNAME'], password: ENV['BASIC_AUTH_PWD']
   def current_user
     OpenStruct.new(id: 1, name: 'Tomáš', email: 'tomas@tomas.cz')
   end
