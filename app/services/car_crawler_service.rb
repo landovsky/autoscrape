148f381d4cae459c062337fed4bcdfa182c27651
Tomas Landovsky <landovsky@gmail.com>
2020-09-02 22:24:01 +0200

    Detect business_lease car sold by hack

--- app/services/car_crawler_service.rb
+++ app/services/car_crawler_service.rb
@@ -1,4 +1,6 @@
 class CarCrawlerService
+  include Utils
+
   def self.call(*cars)
     new.call(cars)
   end
@@ -12,7 +14,7 @@ class CarCrawlerService
 
   def save_raw_page(car)
     raw_page = open_page car
-    car.crawls << Crawl.new(body: raw_page, format: :html)
+    car.crawls << Crawl.new(body: raw_page, format: :html) if raw_page
   end
 
   def open_page(car)
@@ -20,6 +22,9 @@ class CarCrawlerService
 
     if response.body.present?
       response.body
+    elsif response['location'].present? && car.business_lease? && ['velke-a-uzitkove-vozy', 'stredni-vozy'].include?(response['location'].split('/').last)
+      car.car_statuses << CarStatus.new(sales_status: :sold)
+      false
     elsif response['location'].present?
       new_url = response['location']
       response = open_url new_url
@@ -30,10 +35,5 @@ class CarCrawlerService
     else
       raise "Page content missing"
     end
-  end
-
-  def open_url(url)
-    uri      = URI.parse url
-    Net::HTTP.get_response uri
   end
 end
